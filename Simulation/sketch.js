bf0594f45beb4462e01510591f4ad3076ce57e22
Mahbub Alam <mahbubcse96@gmail.com>
2025-03-15 03:24:12 +0600

    Earth will fall down to the sun if it's speed is too low to orbit

--- Simulation/sketch.js
+++ Simulation/sketch.js
@@ -2,26 +2,27 @@ let sun, earth;
 let G = 0.2;
 let orbitPath = [];
 let grid = [];
-let gridSpacing = 30;
-let curvatureIntensity = 0.1;
+let gridSpacing = 50;
+let curvatureIntensity = 0.3;
 let barycenter;
+let deltaTime = 0.1;
 
 function setup() {
   createCanvas(1200, 800, WEBGL);
+  angleMode(DEGREES);
 
-  // Initialize realistic masses (Sun: 99.97% of system mass)
   sun = {
     position: createVector(0, 0),
-    mass: 50000, // ~333,000 Earth masses
+    mass: 50000,
     radius: 40,
     velocity: createVector(0, 0),
   };
 
   earth = {
     position: createVector(300, 0),
-    mass: 1,
-    radius: 5,
-    velocity: createVector(0, 3),
+    mass: 1000,
+    radius: 8,
+    velocity: createVector(0, 3.4),
   };
 
   createGrid();
@@ -38,7 +39,7 @@ function calculateBarycenter() {
 
 function createGrid() {
   grid = [];
-  const range = 1200;
+  const range = 600;
   for (let x = -range; x <= range; x += gridSpacing) {
     let col = [];
     for (let y = -range; y <= range; y += gridSpacing) {
@@ -53,6 +54,10 @@ function draw() {
   lights();
   orbitControl(1, 1, 0.1);
 
+  // Adjust camera view
+  translate(-width / 4, -height / 4);
+  scale(1);
+
   updateSystem();
   drawSpacetimeFabric();
   drawCelestialBodies();
@@ -60,24 +65,26 @@ function draw() {
 }
 
 function updateSystem() {
-  // Calculate gravitational force
-  const force = p5.Vector.sub(earth.position, sun.position);
-  const distance = force.mag();
-  force.normalize();
+  const forceDirection = p5.Vector.sub(sun.position, earth.position);
+  const distance = forceDirection.mag();
+  forceDirection.normalize();
   const strength = (G * sun.mass * earth.mass) / (distance * distance);
-  force.mult(strength);
+  const force = forceDirection.mult(strength);
 
-  // Update velocities for both bodies
-  sun.velocity.add(p5.Vector.div(force, sun.mass));
-  earth.velocity.sub(p5.Vector.div(force, earth.mass));
+  // Fixed syntax for velocity updates
+  sun.velocity.sub(p5.Vector.div(force, sun.mass).mult(deltaTime));
+  earth.velocity.add(p5.Vector.div(force, earth.mass).mult(deltaTime));
 
-  // Update positions
-  sun.position.add(sun.velocity);
-  earth.position.add(earth.velocity);
+  sun.position.add(p5.Vector.mult(sun.velocity, deltaTime));
+  earth.position.add(p5.Vector.mult(earth.velocity, deltaTime));
 
-  // Track orbit relative to barycenter
+  if (distance < sun.radius + earth.radius) {
+    noLoop();
+    console.log("Collision!");
+  }
+
   calculateBarycenter();
-  orbitPath.push(p5.Vector.sub(earth.position, barycenter));
+  orbitPath.push(p5.Vector.sub(earth.position, barycenter).copy());
   if (orbitPath.length > 500) orbitPath.shift();
 
   updateSpacetimeCurvature();
@@ -90,63 +97,66 @@ function updateSpacetimeCurvature() {
       const dSun = dist(point.x, point.y, sun.position.x, sun.position.y);
       const dEarth = dist(point.x, point.y, earth.position.x, earth.position.y);
 
-      // Combined spacetime curvature from both masses
       point.z =
         (-curvatureIntensity * sun.mass) / (dSun + 50) -
-        (curvatureIntensity * earth.mass * 100) / (dEarth + 150);
+        (curvatureIntensity * earth.mass) / (dEarth + 150);
     }
   }
 }
 
 function drawSpacetimeFabric() {
-  stroke(100, 150, 255, 50);
+  stroke(100, 200, 255, 100);
+  strokeWeight(1);
   noFill();
+
   for (let x = 0; x < grid.length - 1; x++) {
     for (let y = 0; y < grid[x].length - 1; y++) {
       beginShape();
-      vertex(grid[x][y].x, grid[x][y].y, grid[x][y].z * 200);
-      vertex(grid[x + 1][y].x, grid[x + 1][y].y, grid[x + 1][y].z * 200);
+      vertex(grid[x][y].x, grid[x][y].y, grid[x][y].z * 300);
+      vertex(grid[x + 1][y].x, grid[x + 1][y].y, grid[x + 1][y].z * 300);
       vertex(
         grid[x + 1][y + 1].x,
         grid[x + 1][y + 1].y,
-        grid[x + 1][y + 1].z * 200
+        grid[x + 1][y + 1].z * 300
       );
-      vertex(grid[x][y + 1].x, grid[x][y + 1].y, grid[x][y + 1].z * 200);
+      vertex(grid[x][y + 1].x, grid[x][y + 1].y, grid[x][y + 1].z * 300);
       endShape(CLOSE);
     }
   }
 }
 
 function drawCelestialBodies() {
-  // Draw Sun (offset from center)
+  // Sun
   push();
-  translate(sun.position.x, sun.position.y, 50);
+  translate(sun.position.x, sun.position.y, 0);
   emissiveMaterial(255, 204, 0);
+  pointLight(255, 204, 0, 0, 0, 0);
   sphere(sun.radius);
   pop();
 
-  // Draw Earth (offset from center)
+  // Earth
   push();
-  translate(earth.position.x, earth.position.y, 50);
-  emissiveMaterial(0, 100, 255);
+  translate(earth.position.x, earth.position.y, 0);
+  emissiveMaterial(100, 150, 255);
   sphere(earth.radius);
   pop();
 
-  // Draw orbital path
+  // Orbit path
   drawOrbitPath();
 }
 
 function drawBarycenter() {
   push();
-  translate(barycenter.x, barycenter.y, 50);
+  translate(barycenter.x, barycenter.y, 0);
   emissiveMaterial(255, 0, 0);
-  sphere(5);
+  sphere(6);
   pop();
 }
 
 function drawOrbitPath() {
   noFill();
   stroke(0, 255, 100);
+  strokeWeight(2);
   beginShape();
   orbitPath.forEach((pos) => {
     const absPos = p5.Vector.add(barycenter, pos);
